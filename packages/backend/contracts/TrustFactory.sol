7a68f4d837b2d534ea155d924b90ef1072a8654f
AAYUSH-GUPTA-coder <60979345+AAYUSH-GUPTA-coder@users.noreply.github.com>
2022-09-22 18:48:14 +0530

    add script

--- packages/backend/contracts/TrustFactory.sol
+++ packages/backend/contracts/TrustFactory.sol
@@ -1,10 +1,11 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.14;
 
 import "./Trust.sol";
 
-contract TrustFactory {
-    struct trustFactoryStruct {
+contract TrustFactory{
+
+     struct trustFactoryStruct {
         uint256 trustIndex;
         uint256 percentage;
         uint256 profileID;
@@ -14,27 +15,19 @@ contract TrustFactory {
         address platformAddress;
     }
 
+
     mapping(uint256 => trustFactoryStruct) public allTrustContracts;
 
     // profile ID, profile ID will be given to junior when he creates the ID on our platform.
     mapping(uint256 => address) public searchProfileID;
 
+
     // number of TrustContracts created
     uint256 public numOfTrustContracts;
 
-    function createTrustContract(
-        uint256 _profileID,
-        uint256 _percentage,
-        uint256 _gigCount
-    ) public {
+    function createTrustContract(uint256 _profileID, uint256 _percentage, uint256 _gigCount) public {
         // Create a new trust contract
-        Trust trust = new Trust(
-            _profileID,
-            _percentage,
-            msg.sender,
-            _gigCount,
-            address(this)
-        );
+        Trust trust = new Trust(_profileID, _percentage, msg.sender, _gigCount, address(this));
 
         // Add the new trust contract to the mapping
         allTrustContracts[numOfTrustContracts] = (
@@ -55,18 +48,21 @@ contract TrustFactory {
         numOfTrustContracts++;
     }
 
-    // get the balance
-    function getContractBalance() public view returns (uint256) {
+     // get the balance 
+    function getContractBalance() public view returns(uint256){
         return address(this).balance;
     }
 
-    function getAddressOfContract() public view returns (address) {
+    function getAddressOfContract() public view returns(address){
         return address(this);
     }
 
+
     // Function to receive Ether. msg.data must be empty
-    receive() external payable {}
+      receive() external payable {}
 
     // Fallback function is called when msg.data is not empty
-    fallback() external payable {}
+      fallback() external payable {}
+
 }
+
---
+++ packages/backend/scripts/trust/deploy.js
@@ -0,0 +1,41 @@
+const { ethers } = require("hardhat");
+require("@nomiclabs/hardhat-etherscan");
+
+async function main() {
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so trustFactoryContract here is a factory for instances of our TrustFactory contract.
+  */
+  const trustFactoryContract = await ethers.getContractFactory("TrustFactory");
+
+  // here we deploy the contract
+  const deployedTrustFactoryContract = await trustFactoryContract.deploy();
+
+  // Wait for it to finish deploying
+  await deployedTrustFactoryContract.deployed();
+
+  // print the address of the deployed contract
+  console.log("TrustFactory Contract Address:", deployedTrustFactoryContract.address);
+
+  console.log("Sleeping.....");
+  // Wait for etherscan to notice that the contract has been deployed
+  await sleep(20000);
+
+  // Verify the contract after deploying
+  await hre.run("verify:verify", {
+    address: deployedTrustFactoryContract.address,
+    constructorArguments: [],
+  });
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
